3d41f091c0cddea9751945394e1e58ccec7d0c7b
Shanice Zaccardi-Richey <shanice.zaccardi.richey@gmail.com>
2020-10-11 13:34:16 -0500

    Added English models to OnBuilder - dummy DB

--- TeamCSharpRegistration/Data/RegistrationDbContext.cs
+++ TeamCSharpRegistration/Data/RegistrationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,64 @@ namespace TeamCSharpRegistration.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Course>().HasData(
+
+                new Course { 
+                    ID = 1,
+                    Department = "ENG",
+                    Number = "101",
+                    Title = "College Composition I",
+                    Description = "College Composition I focuses on the development of writing techniques. Students will develop effective writing styles, writing processes, revision practices, and analytical skills.",
+                    CreditHours = 3,
+                    LectureHours = 3
+                },
+
+                new Course
+                {
+                    ID = 2,
+                    Department = "ENG",
+                    Number = "102",
+                    Title = "College Composition II",
+                    Description = "College Composition II builds on knowledge and skills learned in ENG 101 and primarily focuses on argumentative and persuasive writing techniques. Students will develop effective writing processes, writing styles, research abilities, analytical skills, and argumentative tools.",
+                    CreditHours = 3,
+                    LectureHours = 3
+                },
+
+                new Course
+                {
+                    ID = 3,
+                    Department = "ENG",
+                    Number = "103",
+                    Title = "Report Writing",
+                    Description = "Report Writing builds on knowledge and skills learned in previous writing courses and primarily focuses on the development of writing techniques required in fields such as business, health science, technology, and engineering. Students will develop effective writing styles, writing processes, and analytical skills for business and technical writing.",
+                    CreditHours = 3,
+                    LectureHours = 3
+                },
+
+                new Course
+                {
+                    ID = 4,
+                    Department = "ENG",
+                    Number = "201",
+                    Title = "Introduction to Fiction",
+                    Description = "Introduction to Fiction provides students with an understanding of short and long fiction. Students have the opportunity to study various forms and styles of fiction as well as the major themes and concepts presented within this genre.",
+                    CreditHours = 3,
+                    LectureHours = 3
+                },
+
+                new Course
+                {
+                    ID = 5,
+                    Department = "ENG",
+                    Number = "205",
+                    Title = "American Literature II",
+                    Description = "American Literature II provides a survey of American literature from the Civil War to the present. This course includes the topics of literary criticism, textual reception, as well as historical and cultural context. Various authors and genres will be included.",
+                    CreditHours = 3,
+                    LectureHours = 3
+                }
+
+            );
         }
 
     }
--- TeamCSharpRegistration/Migrations/20201011183132_OnBuilder-English1.Designer.cs
+++ TeamCSharpRegistration/Migrations/20201011183132_OnBuilder-English1.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TeamCSharpRegistration.Data;
 
 namespace TeamCSharpRegistration.Migrations
 {
     [DbContext(typeof(RegistrationDbContext))]
-    partial class RegistrationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201011183132_OnBuilder-English1")]
+    partial class OnBuilderEnglish1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
