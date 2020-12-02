d72742944f5092630c9b744935563a9254d2485f
Shanice Zaccardi-Richey <shanice.zaccardi.richey@gmail.com>
2020-12-02 16:17:43 -0600

    Finished backend work for add to cart feature

--- TeamCSharpRegistration/Controllers/CartController.cs
+++ TeamCSharpRegistration/Controllers/CartController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using TeamCSharpRegistration.Data;
+using TeamCSharpRegistration.Models;
 
 namespace TeamCSharpRegistration.Controllers
 {
@@ -14,18 +17,88 @@ namespace TeamCSharpRegistration.Controllers
     public class CartController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
+        private RegistrationDbContext context { get; set; }
 
-        public CartController(UserManager<IdentityUser> userManager)
+        public CartController(RegistrationDbContext ctx, UserManager<IdentityUser> userManager)
         {
             _userManager = userManager;
+            context = ctx;
         }
 
         [Authorize]
         public IActionResult ViewCart(int sectionID)
         {
             // Shanice - Temporarily using for visual confirmation of data being sent to new view.
-            ViewBag.userid = _userManager.GetUserId(HttpContext.User);
-            return View(sectionID);
+            string userID = _userManager.GetUserId(HttpContext.User);
+            ViewBag.sectionId = sectionID;
+
+            List<CartItem> initialCartItems = new List<CartItem>();
+
+            initialCartItems = context.CartItems
+                .Where(c => c.UserId == userID)
+                .Where(s => s.SectionID == sectionID)
+                .ToList();
+
+            if (initialCartItems.Count == 0)
+            {
+                CartItem cartItem = new CartItem();
+                cartItem.SectionID = sectionID;
+                cartItem.UserId = userID;
+
+                context.Add(cartItem);
+                context.SaveChanges();
+            }
+
+            /*
+            Section section = context.Sections
+                .Where(s => s.ID == sectionID)
+                .ToList()[0];
+            */
+
+            List<CartItem> cartItems = new List<CartItem>();
+
+            cartItems = context.CartItems
+                .Where(c => c.UserId == userID)
+                .ToList();
+
+            List<SectionViewModel> sectionViewModels = new List<SectionViewModel>();
+
+            foreach (CartItem c in cartItems)
+            {
+                SectionViewModel sectionViewModel = new SectionViewModel();
+
+                /*
+                Section currentSection = context.Sections
+                    .Where(s => s.ID == c.SectionID)
+                    .ToList()[0];
+*/
+
+
+                sectionViewModel.Section = context.Sections
+                    .Where(s => s.ID == c.SectionID)
+                    .ToList()[0];
+
+                sectionViewModel.Course = context.Courses
+                    .Where(i => i.ID == sectionViewModel.Section.CourseID)
+                    .ToList()[0];
+
+                sectionViewModel.Instructor = context.Instructors
+                    .Where(i => i.ID == sectionViewModel.Section.InstructorID)
+                    .ToList()[0];
+
+                sectionViewModel.Campus = context.Campuses
+                    .Where(i => i.ID == sectionViewModel.Section.CampusID)
+                    .ToList()[0];
+
+                sectionViewModel.Meetings = context.Meetings
+                    .Where(s => s.SectionID == sectionViewModel.Section.ID)
+                    .ToList();
+
+                sectionViewModels.Add(sectionViewModel);
+            }
+
+
+            return View(sectionViewModels);
         }
     }
 }
