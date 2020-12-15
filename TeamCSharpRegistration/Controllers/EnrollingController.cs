44017b4598f63fb4f7c39318e7585bf572390555
Shanice Zaccardi-Richey <shanice.zaccardi.richey@gmail.com>
2020-12-15 17:14:44 -0600

    Initial enroll setup

--- TeamCSharpRegistration/Controllers/EnrollingController.cs
+++ TeamCSharpRegistration/Controllers/EnrollingController.cs
@@ -2,15 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using TeamCSharpRegistration.Data;
+using TeamCSharpRegistration.Models;
 
 namespace TeamCSharpRegistration.Controllers
 {
     public class EnrollingController : Controller
     {
-        public IActionResult EnrolledClasses()
+        private readonly UserManager<IdentityUser> _userManager;
+        private RegistrationDbContext context { get; set; }
+
+        public EnrollingController(RegistrationDbContext ctx, UserManager<IdentityUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+            context = ctx;
+        }
+        public IActionResult EnrolledClasses(string actionType)
+        {
+            string userID = _userManager.GetUserId(HttpContext.User);
+
+            if (actionType == "enroll")
+            {
+
+                List<CartItem> cartItems = new List<CartItem>();
+
+                cartItems = context.CartItems
+                    .Where(c => c.UserId == userID)
+                    .ToList();
+
+                foreach (CartItem c in cartItems)
+                {
+
+                }
+
+
+
+
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+
+
+            
         }
     }
 }
